d23316ef67cf1fd802478d77d6b358a7349e5504
jake64 <2574270078@qq.com>
2017-08-13 17:21:09 +0800

    first

---
+++ app/src/main/java/binding_demo/coderzheaven/com/bindingdemo/MainActivity.java
@@ -0,0 +1,57 @@
+package binding_demo.coderzheaven.com.bindingdemo;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import binding_demo.coderzheaven.com.bindingdemo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    User user;
+    User2 user2;
+    User3 user3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding = DataBindingUtil.
+                setContentView(this, R.layout.activity_main);
+        user = new User("Coderz", "Heaven");
+        binding.setUser(user);
+        binding.b1.setText("Update");
+        binding.b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Updatin BaseObservable
+                user.setFirstName("New Updated First Name");
+                user.setLastName("New Updated Last Name");
+
+                // Updating ObservableFields
+                user2.firstName.set("Apple");
+                user2.lastName.set("iOS");
+
+                // Updating  Observable Collections
+                user3.name.put(0, "Heaven");
+
+            }
+        });
+
+        // ObservableFields
+        user2 = new User2();
+        user2.firstName.set("Google");
+        user2.lastName.set("Android");
+        binding.setUser2(user2);
+
+        // Observable Collections
+        user3 = new User3();
+        user3.name.put(0, "Coderz");
+        user3.name.put(1, "Heaven");
+        binding.setUser3(user3);
+
+    }
+
+}
---
+++ app/src/main/java/binding_demo/coderzheaven/com/bindingdemo/MyHandler.java
@@ -0,0 +1,13 @@
+package binding_demo.coderzheaven.com.bindingdemo;
+
+import android.util.Log;
+import android.view.View;
+
+import static android.content.ContentValues.TAG;
+
+public class MyHandler {
+
+    public void onClick(View view){
+        Log.i(TAG, "onClick");
+    }
+}
